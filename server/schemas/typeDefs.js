67337792002e0af51210a739796e725b7d6eb0e9
Tmeng <tonymengt@gmail.com>
2022-11-19 13:06:47 -0500

    added jwt and mutation methods

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -28,11 +28,22 @@ const typeDefs = gql`
         friends: [User]
     }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Query {
+        me: User
         users: [User]
         user(username:String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
     }
 
 `;
