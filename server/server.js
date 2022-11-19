9e35715fc4c52bc04f383b1bc67b60248f921098
Tmeng <tonymengt@gmail.com>
2022-11-19 11:32:24 -0500

    implemented graphQL, added get methods

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+// import apollo server
+const { ApolloServer } = require('apollo-server-express');
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// create a new instance of an ApolloServer with he GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  // integrate our ApolloServer with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on ${PORT}`);
+      // log where we can go to test our GQL API
+      console.log(`Use GraphQL a http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+}
+
+// call the async function to start the server
+startApolloServer(typeDefs, resolvers);
