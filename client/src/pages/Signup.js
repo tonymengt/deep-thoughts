24dbeb446300bc4b730c851c884c0036b29bd006
Tmeng <tonymengt@gmail.com>
2022-11-20 18:34:10 -0500

    enabled personalized front end if logged in

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
@@ -28,6 +29,7 @@ const Signup = () => {
         variables: {...formState}
       });
       console.log(data)
+      Auth.login(data.addUser.token);
     } catch (e) {
       console.error(e);
     }
